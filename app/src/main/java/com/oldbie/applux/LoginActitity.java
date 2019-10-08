d635d445a903ece5ecbb0d092ba5a1122774c2d6
John Nguyen <34763517+super0201@users.noreply.github.com>
2019-10-08 15:16:54 +0700

    Revert ">UPDATE: LoginActivity"
    This reverts commit 766bea6f22d2b788c5ea8bb291eb1f05858c9afe.

--- app/src/main/java/com/oldbie/applux/LoginActitity.java
+++ app/src/main/java/com/oldbie/applux/LoginActitity.java
@@ -1,9 +1,7 @@
 package com.oldbie.applux;
 
 import android.annotation.SuppressLint;
-import android.app.AlertDialog;
 import android.app.ProgressDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Handler;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,13 +17,6 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.Task;
 import com.oldbie.applux.model.ServerResponse;
 import com.oldbie.applux.network.NetworkAPI;
 import com.oldbie.applux.network.ServiceAPI;
@@ -35,7 +26,6 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 import static android.widget.Toast.LENGTH_LONG;
-import static android.widget.Toast.LENGTH_SHORT;
 
 public class LoginActitity extends AppCompatActivity {
     private static final String TAG = "LoginActivity";
@@ -44,7 +34,6 @@ public class LoginActitity extends AppCompatActivity {
     EditText etUser,etPass;
     Button btnLogin;
     private NetworkAPI api;
-    GoogleSignInClient mGoogleSignInClient;
 
     //Animation
     Handler handler = new Handler();
@@ -56,20 +45,11 @@ public class LoginActitity extends AppCompatActivity {
         }
     };
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        //Google Login Services
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestEmail()
-                .build();
-
-        // Build a GoogleSignInClient with the options specified by gso.
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+//        setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         //registerUser ServiceAPI and call getJSON from server
         api = ServiceAPI.userService(NetworkAPI.class);
@@ -79,9 +59,6 @@ public class LoginActitity extends AppCompatActivity {
         btnLogin = findViewById(R.id.btnLogin);
         etUser = findViewById(R.id.etUser);
         etPass = findViewById(R.id.etPass);
-        // Set the dimensions of the sign-in button.
-        SignInButton signInButton = findViewById(R.id.sign_in_button);
-        signInButton.setSize(SignInButton.SIZE_STANDARD);
 
         //Animation
         handler.postDelayed(runnable, 2000); //2000 is the timeout for the splash
@@ -104,20 +81,11 @@ public class LoginActitity extends AppCompatActivity {
             }
         });
 
-        //button OnClickListener
+        //button clickListener
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 login();
-            }
-
-        });
-
-        //google button OnClickListener
-        signInButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                signIn();
             }
 
         });
@@ -144,50 +112,45 @@ public class LoginActitity extends AppCompatActivity {
         });
     }
 
-    private void signIn() {
-        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-        startActivityForResult(signInIntent, 101);
-    }
+        public void login(){
+            final ProgressDialog dialog = ProgressDialog.show(LoginActitity.this, "Authenticating",
+                    "Wait a bit mate!", true);
+            dialog.show();
 
-    public void login(){
-        final ProgressDialog dialog = ProgressDialog.show(LoginActitity.this, "Authenticating",
-                "Wait a bit mate!", true);
-        dialog.show();
-
-        //authentication from server
-        new Handler().postDelayed(
-                new Runnable() {
-                    public void run() {
-                        // On complete call either onLoginSuccess or onLoginFailed
-                        if (validate()){
-                            onLoginSuccess();
-                        } else {
-                            onLoginFailed();
+            //authentication from server
+            new Handler().postDelayed(
+                    new Runnable() {
+                        public void run() {
+                            // On complete call either onLoginSuccess or onLoginFailed
+                            if (validate()){
+                                onLoginSuccess();
+                            } else {
+                                onLoginFailed();
+                            }
+                            // onLoginFailed();
+                            dialog.dismiss();
                         }
-                        // onLoginFailed();
-                        dialog.dismiss();
-                    }
-                }, 2000);
-    }
-
-    public void onLoginSuccess() {
-        final String user = etUser.getText().toString();
-        final String pass = etPass.getText().toString();
-
-        Call<ServerResponse> call = api.checkLogin(user, pass);
-        call.enqueue(new Callback<ServerResponse>() {
-            @SuppressLint("ResourceType")
-            @Override
-            public void onResponse(Call<ServerResponse> call, Response<ServerResponse> response) {
-                if (response.body().getResult() == 1) {
-                    Toast.makeText(LoginActitity.this,"Successful", LENGTH_LONG).show();
-                    Intent i = new Intent(getBaseContext(), MainActivity.class);
-                    startActivity(i);
-            } else {
-                    Toast.makeText(LoginActitity.this,"Failed!", LENGTH_LONG).show();
-                Log.e(TAG," Response Error " + response.code());
-            }
+                    }, 2000);
         }
+
+        public void onLoginSuccess() {
+            final String user = etUser.getText().toString();
+            final String pass = etPass.getText().toString();
+
+            Call<ServerResponse> call = api.checkLogin(user, pass);
+            call.enqueue(new Callback<ServerResponse>() {
+                @SuppressLint("ResourceType")
+                @Override
+                public void onResponse(Call<ServerResponse> call, Response<ServerResponse> response) {
+                    if (response.body().getResult() == 1) {
+                        Toast.makeText(LoginActitity.this,"Successful", LENGTH_LONG).show();
+                        Intent i = new Intent(getBaseContext(), MainActivity.class);
+                        startActivity(i);
+                } else {
+                        Toast.makeText(LoginActitity.this,"Failed!", LENGTH_LONG).show();
+                    Log.e(TAG," Response Error " + response.code());
+                }
+            }
 
             @Override
             public void onFailure(Call<ServerResponse> call, Throwable t) {
@@ -197,7 +160,7 @@ public class LoginActitity extends AppCompatActivity {
     }
 
     public void onLoginFailed() {
-        Toast.makeText(getBaseContext(), "Username Or Password Isn't Correct!", LENGTH_SHORT).show();
+        Toast.makeText(getBaseContext(), "Username Or Password Isn't Correct!", Toast.LENGTH_SHORT).show();
         btnLogin.setEnabled(true);
     }
 
@@ -219,77 +182,10 @@ public class LoginActitity extends AppCompatActivity {
     }
 
     @Override
-    public void onStart() {
-        super.onStart();
-        // Check for existing Google Sign In account, if the user is already signed in
-        // the GoogleSignInAccount will be non-null.
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-        updateUI(account);
-    }
-
-    private void updateUI(GoogleSignInAccount account) {
-        if (account != null){
-//            Toast.makeText(LoginActitity.this,"Google already logged in", LENGTH_SHORT).show();
-            Toast.makeText(LoginActitity.this,
-                    account.getDisplayName() + "\n"+
-                            account.getGivenName() + "\n"+
-                            account.getFamilyName() + "\n"+
-                            account.getEmail() + "\n"+
-                            account.getId(), LENGTH_LONG).show();
-            Intent i = new Intent(getBaseContext(), MainActivity.class);
-            startActivity(i);
-            finish();
-        } else {
-            Log.d(TAG, "Not logged in");
-        }
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
-        if (requestCode == 101) {
-            // The Task returned from this call is always completed, no need to attach
-            // a listener.
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            handleSignInResult(task);
-        }
-    }
-
-    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
-        try {
-            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-            // Signed in successfully, show authenticated UI.
-            updateUI(account);
-        } catch (ApiException e) {
-            // The ApiException status code indicates the detailed failure reason.
-            // Please refer to the GoogleSignInStatusCodes class reference for more information.
-            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
-            updateUI(null);
-        }
-    }
-
-    @Override
     public void onBackPressed() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Exit?");
-        builder.setMessage("Are you sure?");
-        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                dialogInterface.dismiss();
-                finish();
-            }
-        });
-        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                Toast.makeText(LoginActitity.this, "Canceled", Toast.LENGTH_SHORT).show();
-            }
-        });
-        AlertDialog alertDialog = builder.create();
-        alertDialog.show();
-//        super.onBackPressed();
+//        Intent intent = new Intent(getBaseContext(), LoginActitity.class);
+//        startActivity(intent);
+        finish();
+        super.onBackPressed();
     }
 }
