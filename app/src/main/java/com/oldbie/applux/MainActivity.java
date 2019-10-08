0319dbe0f297b69b5f8e19cb673d2d0cf85ee0d6
Trong Hung <lehung180010602016@gmail.com>
2019-10-08 13:01:23 +0700

    >>MainActivity<<
    - New > Fragment: Home, TimeTable, User.
    -New AdapterFragment.

--- app/src/main/java/com/oldbie/applux/MainActivity.java
+++ app/src/main/java/com/oldbie/applux/MainActivity.java
@@ -1,13 +1,86 @@
 package com.oldbie.applux;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.tabs.TabLayout;
+import com.oldbie.applux.adapter.FragmentAdapter;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private FragmentAdapter adapter;
+    private ImageView imageView;
+    private CollapsingToolbarLayout collapsingToolbarLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView=findViewById( R.id.ivBackground );
+        collapsingToolbarLayout=findViewById( R.id.collapsingLayout );
+        tabLayout=findViewById( R.id.tabLayout );
+        viewPager=findViewById( R.id.viewPaper );
+
+        adapter = new FragmentAdapter( getSupportFragmentManager(),this );
+        tabLayout.setupWithViewPager( viewPager );
+        viewPager.setAdapter( adapter );
+
+        collapsingToolbarLayout.setTitle( adapter.getPageTitle( 0 ) );
+
+
+        AppBarLayout appbar = findViewById(R.id.appbarMain);
+        appbar.setElevation( 0 );
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        adapter.notifyDataSetChanged();
+
+        viewPager.setOnPageChangeListener( new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                switch (position) {
+                    case 0:
+                        collapsingToolbarLayout.setTitle( adapter.getPageTitle( position ) );
+                        imageView.setImageResource( R.drawable.bg_gradient_1 );
+                        break;
+                    case 1:
+                        collapsingToolbarLayout.setTitle( adapter.getPageTitle( position ) );
+                        imageView.setImageResource( R.drawable.bg_gradient_2 );
+                        break;
+                    case 2:
+                        collapsingToolbarLayout.setTitle( adapter.getPageTitle( position ) );
+                        imageView.setImageResource( R.drawable.bg_gradient_3);
+//                        tabLayout.getTabAt( position ).setIcon( R.drawable.socials_color );
+
+                        break;
+
+                }
+
+
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        } );
     }
 }
---
+++ app/src/main/java/com/oldbie/applux/fragment/FragmentUser.java
@@ -0,0 +1,31 @@
+package com.oldbie.applux.fragment;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.oldbie.applux.R;
+
+import androidx.fragment.app.Fragment;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragmentUser extends Fragment {
+
+
+    public FragmentUser() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate( R.layout.fragment_user, container, false );
+    }
+
+}
