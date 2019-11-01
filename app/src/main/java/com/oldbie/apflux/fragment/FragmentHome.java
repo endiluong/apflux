fca6f06614c6fd81fc0beb9089d8e762589a5687
Unknown <lehung180010602016@gmail.com>
2019-11-01 16:29:48 +0700

    >>Update:Home<<
    - Home: 2 item timetbale, 2 item news

--- app/src/main/java/com/oldbie/apflux/fragment/FragmentHome.java
+++ app/src/main/java/com/oldbie/apflux/fragment/FragmentHome.java
@@ -11,9 +11,12 @@ import android.widget.Toast;
 import com.oldbie.apflux.LoginActivity;
 import com.oldbie.apflux.R;
 import com.oldbie.apflux.adapter.HomeAdapter_news;
+import com.oldbie.apflux.adapter.HomeAdapter_timetable;
 import com.oldbie.apflux.adapter.NewsAdapter;
 import com.oldbie.apflux.model.News;
 import com.oldbie.apflux.model.ResponseNews;
+import com.oldbie.apflux.model.ResponseTimeTable;
+import com.oldbie.apflux.model.TimeTable;
 import com.oldbie.apflux.network.NetworkAPI;
 import com.oldbie.apflux.network.ServiceAPI;
 
@@ -35,7 +38,6 @@ public class FragmentHome extends Fragment {
     public FragmentHome() {
         // Required empty public constructor
     }
-    private String sid,token;
     private RecyclerView rvTimeTable_home,rvNews_home;
     private NetworkAPI api;
 
@@ -43,6 +45,10 @@ public class FragmentHome extends Fragment {
     private HomeAdapter_news adapterNews;
     private News news;
     private ArrayList<News> arrNews;
+
+    //.. Time Table ..//
+    private HomeAdapter_timetable adapterTimetable;
+    private ArrayList<TimeTable> arrTimetable;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -54,6 +60,7 @@ public class FragmentHome extends Fragment {
 
 //        lvMain=(ListView) view.findViewById( R.id.lvMain );
         showData_News();
+        showData_timeTable();
         return view;
     }
 
@@ -64,8 +71,41 @@ public class FragmentHome extends Fragment {
     }
 
     private void showData_timeTable(){
-        sid = LoginActivity.arrSSR.get( 0 ).getStudentId();
-        token = LoginActivity.arrSSR.get( 0 ).getToken();
+        api = ServiceAPI.userService( NetworkAPI.class );
+        final String sid = LoginActivity.arrSSR.get( 0 ).getStudentId();
+        final String token = LoginActivity.arrSSR.get( 0 ).getToken();
+
+        getActivity().runOnUiThread( new Runnable() {
+            @Override
+            public void run() {
+                final String sid = LoginActivity.arrSSR.get( 0 ).getStudentId();
+                final String token = LoginActivity.arrSSR.get( 0 ).getToken();
+
+                Call<ResponseTimeTable> call = api.getAllData( sid,token );
+                call.enqueue( new Callback<ResponseTimeTable>() {
+                    @Override
+                    public void onResponse(Call<ResponseTimeTable> call, Response<ResponseTimeTable> response) {
+//                        Toast.makeText( getContext(),sid+"\n"+token,Toast.LENGTH_SHORT ).show();
+                        if (response.body().getResult()==1){
+                            arrTimetable=response.body().getData();
+                            for (int i=0;i<arrTimetable.size();i++){
+                                adapterTimetable = new HomeAdapter_timetable( getContext(),arrTimetable );
+                                RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
+                                rvTimeTable_home.setLayoutManager(layoutManager);
+                                rvTimeTable_home.setAdapter( adapterTimetable );
+                            }
+                        }else{
+                            Toast.makeText( getContext(), "Error: "+response.body().getResult(), Toast.LENGTH_SHORT ).show();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<ResponseTimeTable> call, Throwable t) {
+
+                    }
+                } );
+            }
+        } );
 
     }
 
