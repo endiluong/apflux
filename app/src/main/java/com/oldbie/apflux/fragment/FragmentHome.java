673f4d910e9e154ca9242971e2747e52399c5436
Unknown <lehung180010602016@gmail.com>
2019-11-03 11:32:09 +0700

    >>Update<<
    - Home: news, timetable. Event click.
    - Edit layout: time table.

--- app/src/main/java/com/oldbie/apflux/fragment/FragmentHome.java
+++ app/src/main/java/com/oldbie/apflux/fragment/FragmentHome.java
@@ -11,9 +11,13 @@ import android.widget.Toast;
 import com.oldbie.apflux.LoginActivity;
 import com.oldbie.apflux.R;
 import com.oldbie.apflux.adapter.HomeAdapter_news;
+import com.oldbie.apflux.adapter.HomeAdapter_timetable;
 import com.oldbie.apflux.adapter.NewsAdapter;
+import com.oldbie.apflux.adapter.RecyclerItemClickListener;
 import com.oldbie.apflux.model.News;
 import com.oldbie.apflux.model.ResponseNews;
+import com.oldbie.apflux.model.ResponseTimeTable;
+import com.oldbie.apflux.model.TimeTable;
 import com.oldbie.apflux.network.NetworkAPI;
 import com.oldbie.apflux.network.ServiceAPI;
 
@@ -22,8 +26,10 @@ import java.util.Objects;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -44,6 +50,10 @@ public class FragmentHome extends Fragment {
     private News news;
     private ArrayList<News> arrNews;
 
+    //.. TIME TABLE ..//
+    private HomeAdapter_timetable adapterTimetable;
+    private ArrayList<TimeTable> arrTimetable;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -54,6 +64,7 @@ public class FragmentHome extends Fragment {
 
 //        lvMain=(ListView) view.findViewById( R.id.lvMain );
         showData_News();
+        showData_timeTable();
         return view;
     }
 
@@ -64,8 +75,52 @@ public class FragmentHome extends Fragment {
     }
 
     private void showData_timeTable(){
-        sid = LoginActivity.arrSSR.get( 0 ).getStudentId();
-        token = LoginActivity.arrSSR.get( 0 ).getToken();
+        api = ServiceAPI.userService( NetworkAPI.class );
+
+        getActivity().runOnUiThread( new Runnable() {
+            @Override
+            public void run() {
+                final String sid = LoginActivity.arrSSR.get( 0 ).getStudentId();
+                final String token = LoginActivity.arrSSR.get( 0 ).getToken();
+
+                Call<ResponseTimeTable> call = api.getAllData( sid,token );
+                call.enqueue( new Callback<ResponseTimeTable>() {
+                    @Override
+                    public void onResponse(Call<ResponseTimeTable> call, Response<ResponseTimeTable> response) {
+//                        Toast.makeText( getContext(),sid+"\n"+token,Toast.LENGTH_SHORT ).show();
+                        if (response.body().getResult()==1){
+                            arrTimetable=response.body().getData();
+                            for (int i=0;i<arrTimetable.size();i++){
+                                adapterTimetable = new HomeAdapter_timetable( getContext(),arrTimetable );
+                                RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
+                                rvTimeTable_home.setLayoutManager(layoutManager);
+                                rvTimeTable_home.setAdapter( adapterTimetable );
+                            }
+                        }else{
+                            Toast.makeText( getContext(), "Error: "+response.body().getResult(), Toast.LENGTH_SHORT ).show();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<ResponseTimeTable> call, Throwable t) {
+
+                    }
+                } );
+            }
+        } );
+
+        rvTimeTable_home.addOnItemTouchListener( new RecyclerItemClickListener( getContext(), new RecyclerItemClickListener.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+
+                getActivity().getSupportFragmentManager().popBackStack();
+                transaction.replace(R.id.fragment, new FragmentTimetable());
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        } ) );
 
     }
 
@@ -100,5 +155,18 @@ public class FragmentHome extends Fragment {
                 } );
             }
         } );
+
+        rvNews_home.addOnItemTouchListener( new RecyclerItemClickListener( getContext(), new RecyclerItemClickListener.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+
+                getActivity().getSupportFragmentManager().popBackStack();
+                transaction.replace(R.id.fragment, new FragmentNews());
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        } ) );
     }
 }
