292be77247ff38f4a8c93d86d84f0ff14906f51e
Unknown <lehung180010602016@gmail.com>
2019-12-01 20:37:03 +0700

    >>Mark Detail<<
    edit 2rd

--- app/src/main/java/com/oldbie/apflux/fragment/FragmentMark.java
+++ app/src/main/java/com/oldbie/apflux/fragment/FragmentMark.java
@@ -1,5 +1,6 @@
 package com.oldbie.apflux.fragment;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,16 +8,26 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import com.oldbie.apflux.LoginActivity;
 import com.oldbie.apflux.R;
 import com.oldbie.apflux.adapter.MarkAdapter;
+import com.oldbie.apflux.model.GetMark;
 import com.oldbie.apflux.model.Mark;
+import com.oldbie.apflux.model.ResponseGetMark;
 import com.oldbie.apflux.model.ResponseMark;
 import com.oldbie.apflux.network.NetworkAPI;
 import com.oldbie.apflux.network.ServiceAPI;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -28,8 +39,13 @@ public class FragmentMark extends Fragment {
 
     private ListView lvMark;
     private NetworkAPI api;
-    private MarkAdapter markAdapter;
+    private MarkAdapter adapter;
     private ArrayList<Mark> arrMark = new ArrayList<>();
+    private int index = 0;
+
+    //TEST..
+    private ArrayList<GetMark> arrGetMark;
+    //..
 
     public FragmentMark() {
         // Required empty public constructor
@@ -53,30 +69,61 @@ public class FragmentMark extends Fragment {
             public void run() {
             final String checkId = LoginActivity.arrSSR.get(0).getStudentId();
             final String token = LoginActivity.arrToken.get(0).getToken();
-            Call<ResponseMark> call = api.getMark(checkId, token);
-            call.enqueue(new Callback<ResponseMark>() {
+            Call<ResponseGetMark> call = api.getAllMark( checkId,index,token  );
+            call.enqueue( new Callback<ResponseGetMark>() {
                 @Override
-                public void onResponse(Call<ResponseMark> call, Response<ResponseMark> response) {
-                    if(response.body().getResult()==1){
-                        arrMark=response.body().getData();
-                        for(int i=0;i<arrMark.size();i++){
-                            markAdapter=new MarkAdapter(getContext(), arrMark);
-                            lvMark.setAdapter(markAdapter);
+                public void onResponse(Call<ResponseGetMark> call, Response<ResponseGetMark> response) {
+                    if (response.body().getResult().equals( 1 )){
+                        arrGetMark=response.body().getData();
+                        for(int i=0;i<arrGetMark.size();i++){
+//                            String toastTest = arrGetMark.get( i ).getmSubjectName();
+//                            Toast.makeText( getContext(),toastTest,Toast.LENGTH_SHORT ).show();
+                            adapter = new MarkAdapter( getContext(),arrGetMark );
+                            lvMark.setAdapter( adapter );
+                            onClickItem();
                         }
-                    } else {
-                        Toast.makeText(getContext(),"Errors!",Toast.LENGTH_SHORT).show();
+                    }else{
+                        Toast.makeText( getContext(),"please check connection",Toast.LENGTH_SHORT ).show();
                     }
+
                 }
 
                 @Override
-                public void onFailure(Call<ResponseMark> call, Throwable t) {
-
+                public void onFailure(Call<ResponseGetMark> call, Throwable t) {
+                    Toast.makeText( getContext(),"please check connection",Toast.LENGTH_SHORT ).show();
                 }
-            });
+            } );
             }
         });
     }
 
+    //.. EVENT CLICK ON ITEM ..//
+    public void onClickItem(){
+        lvMark.setOnItemClickListener( new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                ArrayList<GetMark.GetMarkDetail> details = arrGetMark.get( position ).getArrayList();
+                String MarkType,MarkDetail,Perc;
+                for (int i=0;i<details.size();i++){
+                    MarkType = details.get( i ).getmMarkType();
+                    MarkDetail = details.get( i ).getmPercentage();
+                    Perc = details.get( i ).getmMarkDetail();
+                    Toast.makeText( getContext(), MarkType+"\n"+ Perc+"\n"+MarkDetail,Toast.LENGTH_SHORT).show();
+
+                }
+
+//                AlertDialog.Builder builder = new AlertDialog.Builder( getContext() );
+//                builder.setTitle( arrGetMark.get( position ).getmSubjectName() )
+//                        .setMessage( MarkType+"\n"+ Perc+"\n"+MarkDetail+"\n")
+//                        .setCancelable( true ).create().show();
+            }
+        } );
+
+
+
+
+
+    }
     @Override
     public void onResume() {
         super.onResume();
